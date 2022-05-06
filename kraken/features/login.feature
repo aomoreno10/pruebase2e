d8efeaf161f6853f8d4a3258c82e47713e06629f
Ondinne <j.padillag@uniandes.edu.co>
2022-05-06 11:34:51 -0500

    Publicar un post OK

--- kraken/features/login.feature
+++ kraken/features/login.feature
@@ -1,7 +1,18 @@
 Feature: Registrarse en la pagina de ghost
 
 @user1 @web
-Scenario: Como usuario inicio sesion
+Scenario: Como administrador inicio sesion
+  Given I navigate to page "http://localhost:2368/ghost/"
+  And I wait for 5 seconds
+  When I enter email "<USERNAME1>"
+  And I wait for 2 seconds
+  And I enter password "<PASSWORD1>"
+  And I wait for 2 seconds
+  Then I click next
+  And I wait for 2 seconds
+
+@user2 @web
+Scenario: Crear un post
   Given I navigate to page "http://localhost:2368/ghost/"
   And I wait for 5 seconds
   When I enter email "<USERNAME1>"
@@ -9,4 +20,19 @@ Scenario: Como usuario inicio sesion
   And I enter password "<PASSWORD1>"
   And I wait for 2 seconds
   And I click next
+  And I wait for 2 seconds
+  And I click publish a post
+  And I wait for 2 seconds
+  And I click on the redact post title inputbox
+  And I wait for 2 seconds
+  And I enter post title'Mi primer post'
+  And I wait for 2 seconds
+  And I click on the redact post body inputbox
+  And I wait for 2 seconds
+  And I enter post body'Este es un post de prueba'
+  And I click on the publish button
+  And I wait for 2 seconds
+  And I click on the publish button again
+  And I wait for 2 seconds
+  And I publish the post
   And I wait for 2 seconds
--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -10,7 +10,47 @@ When('I enter password {kraken-string}', async function (password) {
     return await element.setValue(password);
 });
 
-Then('I click next', async function() {
+When('I click next', async function() {
     let element = await this.driver.$('#ember11');
     return await element.click();
-})
+});
+
+When('I click publish a post', async function() {
+    let element = await this.driver.$('#ember37');
+    return await element.click();
+});
+
+When('I click on the redact post title inputbox', async function() {
+    let element = await this.driver.$('#ember63');
+    return await element.click();
+});
+
+When('I enter post title{string}', async function (post_title) {
+    let element = await this.driver.$('#ember63');
+    return await element.setValue(post_title);
+});
+
+When('I click on the redact post body inputbox', async function() {
+    let element = await this.driver.$('#ember64');
+    return await element.click();
+});
+
+When('I enter post body{string}', async function (post_body) {
+    let element = await this.driver.$('#ember64');
+    return await element.setValue(post_body);
+});
+
+Then('I click on the publish button', async function () {
+    let element = await this.driver.$('#ember71 > span');
+    return await element.click();
+});
+
+Then('I click on the publish button again', async function () {
+    let element = await this.driver.$('#ember79 > span');
+    return await element.click();
+});
+
+Then('I publish the post', async function () {
+    let element = await this.driver.$('#ember81 > span');
+    return await element.click();
+});
